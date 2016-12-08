a1470408b3adad8ef8c2995f14c9ce1be9d098b7
Eric Myhre <hash@exultant.us>
2016-12-08 12:53:59 -0500

    clear out, starting over and trying lfs instructions.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- allclean.sh
+++ allclean.sh
@@ -3,4 +3,3 @@ rm .reppl
 reppl init
 reppl put hash ubuntu-base     aLMH4qK1EdlPDavdhErOs0BPxqO0i6lUaeRE4DuUmnNMxhHtF56gkoeSulvwWNqT
 reppl put hash ubuntu-base+gcc 0-wPN--PLP80sClmWF_GtUZMckIEiSIC9nCcbzwun55ucvZA2P3ui9rx63yMVacc
-reppl eval sup.frm
