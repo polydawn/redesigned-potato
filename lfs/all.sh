c054d725218eeb16197073212d81a20e661b367c
Eric Myhre <hash@exultant.us>
2017-04-15 13:21:22 +0200

    Additional whitespace style police.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- lfs/all.sh
+++ lfs/all.sh
@@ -6,9 +6,13 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 mkdir -p wares
 mkdir -p debug
 
+
+
 ### Phase -1: "Host" image construction.
 reppl put hash  ubuntu-base  aLMH4qK1EdlPDavdhErOs0BPxqO0i6lUaeRE4DuUmnNMxhHtF56gkoeSulvwWNqT  --warehouse=http+ca://repeatr.s3.amazonaws.com/assets/
 reppl eval ubuntu+lfs-deps.frm
+
+
 
 ### Phase 0: building basics, the first time.
 reppl put hash  binutils-src  m2-ZAxYvz_XMuWPj0lay3vsxf-pmrghkLQUeG83JzJfEg8eZ3d4q17eqq1Z5OGdY  --warehouse=http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.bz2
@@ -23,5 +27,5 @@ reppl eval lfs-002-gcc.frm
 reppl put hash  kernel-src  gFPeFwgxUTG_1wkAcvQhLLK0KE_kXlVzve7VOAJiIdF70Khrpb28aVVIPoDWPUEo  --warehouse=https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.7.2.tar.xz
 reppl eval lfs-003-kernelheaders.frm
 
-reppl put hash glibc-src  EuSaUYVTHP-qIzxgY2vDdArjzitbFQQ8G-YDe2_dZU48zGd0seLcBpb6kGElk62Z --warehouse=http://ftp.gnu.org/gnu/glibc/glibc-2.24.tar.xz
+reppl put hash  glibc-src  EuSaUYVTHP-qIzxgY2vDdArjzitbFQQ8G-YDe2_dZU48zGd0seLcBpb6kGElk62Z --warehouse=http://ftp.gnu.org/gnu/glibc/glibc-2.24.tar.xz
 reppl eval lfs-004-glibc.frm
